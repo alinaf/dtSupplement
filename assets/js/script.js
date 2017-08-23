1e372ad6a0c3b90481def6d3dfe105ccb677fdce
Alina Abidi <aabidi@usc.edu>
2017-08-23 12:27:35 -0700

    Click to pause audio

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,32 +6,67 @@ $(document).ready(function(){ // begin document.ready block
 
 	$("#gallery img").click(function(){
 
+		var audioPlayer = document.getElementById("mysound");
+		var source = audioPlayer.getAttribute("src");
+		console.log(source);
+
 		var bag = $(this).attr("num");
-		console.log(bag);
 		var myaudio = audioList[bag];
-		console.log(myaudio);
 		$("#mysound").attr("src", myaudio);
 		$("#mysound").get(0).play();
+
+	
+		//if click again, then pause 
+		if (source == myaudio) {
+			console.log("yes")
+			$("#mysound").get(0).pause();
+
+			// Reset so you can click again
+			$("#mysound").attr("src", "");
+		} 
 	});
 
 	$("#gallery2 img").click(function(){
 
+		var audioPlayer = document.getElementById("mysound");
+		var source = audioPlayer.getAttribute("src");
+		console.log(source);
+
 		var bag = $(this).attr("num");
-		console.log(bag);
 		var myaudio = audioList[bag];
-		console.log(myaudio);
 		$("#mysound").attr("src", myaudio);
 		$("#mysound").get(0).play();
+
+	
+		//if click again, then pause 
+		if (source == myaudio) {
+			console.log("yes")
+			$("#mysound").get(0).pause();
+
+			// Reset so you can click again
+			$("#mysound").attr("src", "");
+		} 
 	});
 
 	$("#gallery3 img").click(function(){
 
+		var audioPlayer = document.getElementById("mysound");
+		var source = audioPlayer.getAttribute("src");
+		console.log(source);
+
 		var bag = $(this).attr("num");
-		console.log(bag);
 		var myaudio = audioList[bag];
-		console.log(myaudio);
 		$("#mysound").attr("src", myaudio);
 		$("#mysound").get(0).play();
-	});
 
+	
+		//if click again, then pause 
+		if (source == myaudio) {
+			console.log("yes")
+			$("#mysound").get(0).pause();
+
+			// Reset so you can click again
+			$("#mysound").attr("src", "");
+		} 
+	});
 }); //end document.ready block
